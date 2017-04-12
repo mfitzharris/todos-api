e87b7d145af56ead439a7138afff15406dacd8c6
mfitzharris <molly.fitzharris@gmail.com>
2017-04-11 21:58:56 -0500

    Define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :todos do
+    resources :items
+  end
 end
+
+#     Prefix Verb   URI Pattern                         Controller#Action
+# todo_items GET    /todos/:todo_id/items(.:format)     items#index
+#            POST   /todos/:todo_id/items(.:format)     items#create
+#  todo_item GET    /todos/:todo_id/items/:id(.:format) items#show
+#            PATCH  /todos/:todo_id/items/:id(.:format) items#update
+#            PUT    /todos/:todo_id/items/:id(.:format) items#update
+#            DELETE /todos/:todo_id/items/:id(.:format) items#destroy
+#      todos GET    /todos(.:format)                    todos#index
+#            POST   /todos(.:format)                    todos#create
+#       todo GET    /todos/:id(.:format)                todos#show
+#            PATCH  /todos/:id(.:format)                todos#update
+#            PUT    /todos/:id(.:format)                todos#update
+#            DELETE /todos/:id(.:format)                todos#destroy
